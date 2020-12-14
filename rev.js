45fd52311d51a372ca0f8bf82ac86d8807ad4acb
Mamata24 <nmamta240@gmail.com>
2020-12-14 16:59:38 +0530

    final commit

---
+++ rev.js
@@ -0,0 +1,12 @@
+var v = 10;
+(() => {
+    varia_3 = 35;
+    console.log(varia_3);
+    var varia_3 = 45;
+    varia_2 = 15;
+    console.log(v)
+})();
+
+console.log(varia_2);
+console.log(varia_3);
+var v = 30;
--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -3,7 +3,8 @@ import TodoList from './TodoList'
 import Box from '@material-ui/core/Box';
 import Swal from 'sweetalert'
 function TodoInput() {
-    const [todo, setTodo] = useState({ title: "", done: false, tag:"" })
+    const [todo, setTodo] = useState({ title: "", done: false, tag: [] })
+    const [tag,setTag]  = useState("")
     const [todoArray, setTodoArray] = useState([])
     const [list, setList] = useState([])
     let todos = localStorage.hasOwnProperty("todos") ? JSON.parse(localStorage.getItem("todos")) : []
@@ -20,7 +21,9 @@ function TodoInput() {
         const { name } = e.target
         if (e.key === "Enter" || name === "addTodo") {
             if (todo.title !== "") {
+                todo.tag = tag
                 todos.unshift(todo)
+                setTodoArray(todos)
                 localStorage.setItem('todos', JSON.stringify(todos))
                 setTodo({ title: "", done: false })
             }
@@ -70,21 +73,17 @@ function TodoInput() {
         setList(b)
     }
 
-    const handleSelect = (e) => {
-    const { name } = e.target
-        if (e.key === "Enter" || name === "addTodo") {
-            setTodo({
-                ...todo,
-                tag: e.target.value
-            })
-        }
-    }
+    
 
     const handleTag = (e) => {
-        console.log(e.target.textContent.slice(1))
-        const b = todos.filter(todo => todo["tag"] === e.target.textContent.slice(1))
+        const b = todos.filter(todo => todo["tag"] === e.target.textContent) 
         setList(b)
     }
+
+    const handleTextarea = (e) => {
+        setTag(e.target.value)
+    }
+
     
     
     return (
@@ -95,12 +94,7 @@ function TodoInput() {
             </h1>
             <Box borderRadius="10px" component="div" display="inline" p={1} m={1} bgcolor="background.paper">
             <input type="text" name="todo" placeholder="Add task" value={todo.title} onKeyPress={createTodo} onChange={onChange} />
-             <select style={{marginLeft:"10px"}} value={todo.tag} onChange={handleSelect} onKeyPress={handleSelect}>
-                <option value="work">Work</option>
-                <option value="backend">Backend</option>
-                <option selected value="frontend">Frontend</option>
-                <option value="daily stuff">Daily Stuff</option>
-            </select>    
+             <input type="text" className="textArea" cols="20" rows="0" onChange={handleTextarea} value={tag} placeholder="use space to separate tags"/>  
             <button className="btn-addTodo add" type="button" name="addTodo" onClick={createTodo}>Add</button>
             </Box>
             </div>
@@ -113,11 +107,7 @@ function TodoInput() {
             </div>
             <div>
                 <div className="tag" onClick={handleTag}>
-                    <span>#all</span>
-                    <span>#work</span>
-                    <span>#backend</span>
-                    <span>#frontend</span>
-                    <span>#daily_stuff</span>
+                    {list.map(item => <span>{item.tag}</span>)}
                 </div>
             </div>
             <TodoList todoArray={list} completeTodo={completeTodo} />

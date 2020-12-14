e4ae1c6dd77ecb6a1f842e0c66ade64999920916
Mamata24 <nmamta240@gmail.com>
2020-12-14 14:41:37 +0530

    rendering the todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './Components/styles.css'
+import TodoInput from './Components/TodoInput';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoInput/>
+    </div>
+  );
+}
+
+export default App;
